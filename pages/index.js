bd9cd2adf31e27f50f328fee74f6a4c0684b8490
sergeyman <sergey_man@ukr.net>
2020-06-15 01:23:51 +0300

    Initial commit for working next.js app

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+// ./pages/index.js
+
+// Import React
+import React from 'react'
+
+// Export an anonymous arrow function
+// which returns the template
+export default () => (
+  <h1>This is just so easy 123!</h1>
+)
