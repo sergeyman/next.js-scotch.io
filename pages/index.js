bb6ef0dfedc0ce92ff6dd1ddbbe3d54989222f71
sergeyman <sergey_man@ukr.net>
2020-06-15 01:27:51 +0300

    Adding Head Next.js component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
 // ./pages/index.js
 
 // Import React
-import React from 'react'
+import React from "react";
+// Import the Head Component
+import Head from "next/head";
 
 // Export an anonymous arrow function
 // which returns the template
 export default () => (
-  <h1>This is just so easy 123!</h1>
-)
+  <div>
+    <Head>
+      <title>League Table</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/purecss@0.6.1/build/pure-min.css"
+      />
+    </Head>
+    <h1>This is just so easy 123!</h1>
+  </div>
+);
